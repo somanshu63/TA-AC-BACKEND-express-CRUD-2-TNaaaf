4c2af7563cc8fbab640996eee2fa0f6dc84b1c04
somanshu63 <somanshu63@gmail.com>
2022-09-06 17:04:53 +0530

    express crud with association assignment 1

--- block-BNaadl/blog/routes/articles.js
+++ block-BNaadl/blog/routes/articles.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var Article = require('../model/article');
 
 /* GET articles listing. */
-
+//create article form
 router.get('/new', (req, res) => {
   console.log('hi')
   res.render('form');
 });
 
+//show single article
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
   Article.findById(id, (err, article) => {
@@ -17,22 +18,27 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+//get article form to update
 router.get('/:id/edit', (req, res, next) => {
   var id = req.params.id;
   Article.findById(id, (err, article) => {
+    article.tags = article.tags.join(' ');
     if(err) return next(err);
     res.render('updateform', { article })
   });
 });
 
+//update article
 router.post('/:id/edit', (req, res, next) => {
   var id = req.params.id;
+  req.body.tags = req.body.tags.trim().split(' ');
   Article.findByIdAndUpdate(id, req.body, (err, updatedArticle) => {
     if(err) return next(err);
     res.redirect('/articles');
   });
 });
 
+//addlikes
 router.get('/:id/addlike/:likes', (req, res, next) => {
   var id = req.params.id;
   var likes = Number(req.params.likes);
@@ -42,6 +48,7 @@ router.get('/:id/addlike/:likes', (req, res, next) => {
   });
 });
 
+//sublikes
 router.get('/:id/sublike/:likes', (req, res, next) => {
   var id = req.params.id;
   var likes = Number(req.params.likes);
@@ -51,6 +58,7 @@ router.get('/:id/sublike/:likes', (req, res, next) => {
   });
 });
 
+//delete article
 router.get('/:id/delete', (req, res, next) => {
   var id = req.params.id;
   Article.findByIdAndDelete(id, (err, article) => {
@@ -59,6 +67,7 @@ router.get('/:id/delete', (req, res, next) => {
   });
 });
 
+//list all articles
 router.get('/', function(req, res, next) {
   Article.find({}, (err, articles) => {
     if(err) return next(err);
@@ -66,7 +75,9 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//create article
 router.post('/', (req, res) => {
+  req.body.tags = req.body.tags.trim().split(' ');
   Article.create(req.body, (err, capturedarticle) => {
     if(err) res.redirect('/articles/new');
         res.redirect(`articles`);
